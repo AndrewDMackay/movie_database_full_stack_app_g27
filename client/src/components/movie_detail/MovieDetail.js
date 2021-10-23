c9c9eea09c74834b9c7203c8383bafb37d698235
Vinnie <vincentbennis@vinnies-air.home>
2021-10-23 18:47:17 +0100

    6th commit, container / list / listitem / itemdeatil worked on

---
+++ client/src/components/movie_detail/MovieDetail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MovieDetail = ({ selectedMovie }) => {
+
+    return (
+
+        <div>
+            <h2>{selectedMovie.Title}</h2>
+            <img src={selectedMovie.Poster} alt=""/>
+            <div>
+                <p>{selectedMovie.Plot}</p>
+                <p>{selectedMovie.Ratings[0].Source}: {selectedMovie.Ratings[0].Value}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieDetail;
--- client/src/container/MovieContainer.js
+++ client/src/container/MovieContainer.js
@@ -1,18 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import MovieList from "../components/main_movie_page/MovieList"
+import MovieDetail from '../components/movie_detail/MovieDetail'
 
 const MovieContainer = () => {
     const [movies, setMovies] = useState([])
+    const [selectedMovie, setSelectedMovie] = useState(null)
 
-    // searches API by movie title
-    const getMoviesByTitle = function(movieTitle){
+    useEffect(() => {
+        getMoviesByTitle();
+    });
 
-        fetch(`http://www.omdbapi.com/?t=$batmanapikey=30f7090a`)
-        .then(res => res.json())
-        .then(movies => setMovies(movies))
+    // searches API by movie title (hardcoded for batman for example)
+    const getMoviesByTitle = function () {
+        fetch(`https://www.omdbapi.com/?s=batman&apikey=30f7090a`)
+            .then(res => res.json())
+            .then(data => setMovies(data.Search))
+    }
+
+    // searches API by movie imdb id after one has been selected form the movie list
+    const onMovieClick = function (movie) {
+        fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=30f7090a`)
+            .then(res => res.json())
+            .then(data => setSelectedMovie(data))
     }
 
     // const getMoviesByGenre = function(genre){
-
     //     fetch(`http://www.omdbapi.com/?t=${genre}apikey=30f7090a`)
     //     .then(res => res.json())
     //     .then(movies => setMovies(movies))
@@ -26,12 +38,13 @@ const MovieContainer = () => {
     //     deleteUserReview(id)
     // }
 
-    return(
+    return (
         <>
-        <h1>RateMyPoo.com</h1>
-
+            <h1>This is the MovieContainer</h1>
+            {!selectedMovie ? <MovieList movies={movies} onMovieClick={onMovieClick} /> : null}
+            {selectedMovie ? <MovieDetail selectedMovie={selectedMovie} /> : null}
         </>
     )
-} 
+}
 
 export default MovieContainer;
