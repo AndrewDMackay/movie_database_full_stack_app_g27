2af1b18a84ddb5926b58f95334c65fc982aa2af1
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-24 20:02:53 +0100

    AM, 6th commit feature/css_general_styling_24_10_21

--- client/src/components/movie_detail/MovieDetail.js
+++ client/src/components/movie_detail/MovieDetail.js
@@ -5,15 +5,17 @@ import React from 'react';
 const MovieDetail = ({ selectedMovie }) => {
 
     return (
-
-        <div className="movie-detail">
-            <h2>{selectedMovie.Title}</h2>
-            <img src={selectedMovie.Poster} alt=""/>
-            <div>
-                <p>{selectedMovie.Plot}</p>
-                <p>{selectedMovie.Ratings[0].Source}: {selectedMovie.Ratings[0].Value}</p>
+        
+        <div className="movie-detail-container">
+            <div className="movie-detail">
+                <h2>{selectedMovie.Title}</h2>
+                <img className="movie-detail-poster" src={selectedMovie.Poster} alt=""/>
+                <div>
+                    <p>{selectedMovie.Plot}</p>
+                    <p>{selectedMovie.Ratings[0].Source}: {selectedMovie.Ratings[0].Value}</p>
+                </div>
             </div>
-        </div>
+        </div>    
     )
 }
 
