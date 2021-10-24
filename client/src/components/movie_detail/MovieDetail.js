6ef27d191eda71846df14832c016d54ee6c06df6
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-24 20:29:23 +0100

    AM, 7th commit feature/css_general_styling_24_10_21

--- client/src/components/movie_detail/MovieDetail.js
+++ client/src/components/movie_detail/MovieDetail.js
@@ -2,7 +2,12 @@
 import React from 'react';
 
 
-const MovieDetail = ({ selectedMovie }) => {
+const MovieDetail = ({ selectedMovie, onHomeClick }) => {
+
+
+    const handleClick = function(){
+        onHomeClick();
+    }
 
     return (
         
@@ -14,6 +19,7 @@ const MovieDetail = ({ selectedMovie }) => {
                     <p>{selectedMovie.Plot}</p>
                     <p>{selectedMovie.Ratings[0].Source}: {selectedMovie.Ratings[0].Value}</p>
                 </div>
+                <button className="custom-button" onClick={handleClick}>HOME</button>
             </div>
         </div>    
     )
--- client/src/container/MovieContainer.js
+++ client/src/container/MovieContainer.js
@@ -10,6 +10,7 @@ const MovieContainer = () => {
     const [movies, setMovies] = useState([])
     const [selectedMovie, setSelectedMovie] = useState(null)
 
+
     // searches API by movie title (hardcoded for batman for example)
     const onTitleSearched = function (title) {
         setSelectedMovie(null)
@@ -19,6 +20,7 @@ const MovieContainer = () => {
             .then(data => setMovies(data.Search))
     }
 
+
     // searches API by movie imdb id after one has been selected form the movie list
     const onMovieClick = function (movie) {
         fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=30f7090a`)
@@ -26,11 +28,19 @@ const MovieContainer = () => {
             .then(data => setSelectedMovie(data))
     }
 
+
     // const getMoviesByGenre = function(genre){
     //     fetch(`http://www.omdbapi.com/?t=${genre}apikey=30f7090a`)
     //     .then(res => res.json())
     //     .then(movies => setMovies(movies))
     // }
+
+    
+
+    const onHomeClick = function(){
+        setSelectedMovie(null);
+    }
+
 
     return (
         <>
@@ -38,7 +48,7 @@ const MovieContainer = () => {
                 <h1>THIS IS THE MOVIE CONTAINER</h1>
                 <SearchBar onTitleSearched={onTitleSearched} />
                 {!selectedMovie ? <MovieList movies={movies} onMovieClick={onMovieClick} /> : null}
-                {selectedMovie ? <MovieDetail selectedMovie={selectedMovie} /> : null}
+                {selectedMovie ? <MovieDetail selectedMovie={selectedMovie} onHomeClick={onHomeClick} /> : null}
             </div>
         </>
     )
