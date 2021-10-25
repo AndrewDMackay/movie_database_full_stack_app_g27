4c66bc8d18ac1dd39a65540eb7b150d09953333a
Vinnie <vincentbennis@vinnies-air.home>
2021-10-25 20:50:30 +0100

    VB fetch changed to only return movies

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,11 @@
 
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
+import SearchBar from "./main_movie_page/SearchBar";
 
 
-const NavBar = () => {
-    
+const NavBar = ({ onTitleSearched }) => {
+
     // const history = useHistory();
 
     // const goBack = () => {
@@ -13,27 +14,30 @@ const NavBar = () => {
 
 
     return (
-            
+
         <>
-        <div className="main-nav-bar">
-            <ul>
-                <li>
-                    <Link to="/">HOME</Link>
-                </li>
-                <li>
-                    <Link to="/genres">MOVIES BY GENRE</Link>
-                </li>
-                <li>
-                    <Link to="/year">MOVIES BY YEAR</Link>
-                </li>
-                <li>
-                    <Link to="/toprated">TOP RATED MOVIES</Link>
-                </li>
-                <li>
-                    <Link to="/favourites">FAVOURITES</Link>
-                </li>
-            </ul>
-        </div>
+            <div className="main-nav-bar">
+                <ul>
+                    <li>
+                        <Link to="/">HOME</Link>
+                    </li>
+                    <li>
+                        <Link to="/genres">MOVIES BY GENRE</Link>
+                    </li>
+                    <li>
+                        <Link to="/year">MOVIES BY YEAR</Link>
+                    </li>
+                    <li>
+                        <Link to="/toprated">TOP RATED MOVIES</Link>
+                    </li>
+                    <li>
+                        <Link to="/favourites">FAVOURITES</Link>
+                    </li>
+                    <li>
+                        <SearchBar onTitleSearched={onTitleSearched} />
+                    </li>
+                </ul>
+            </div>
         </>
     )
 };
