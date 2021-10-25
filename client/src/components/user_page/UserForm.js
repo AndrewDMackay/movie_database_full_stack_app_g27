c7b2876ed9877f2f81e40c18c9e1f1d1090de6f8
Vinnie <vincentbennis@Vinnies-MacBook-Air.local>
2021-10-25 15:14:18 +0100

    KS / VB user list & login issue fixed

--- client/src/components/user_page/UserForm.js
+++ client/src/components/user_page/UserForm.js
@@ -1,52 +1,52 @@
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
-    const UserForm = ({onNewUserSubmit}) => {
+const UserForm = ({ onNewUserSubmit }) => {
 
-        const [name, setName] = useState("")
-        const [email, setEmail] = useState("")
-        const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-        const handleNameChange = (event) => {
-            setName(event.target.value)
+    const handleNameChange = (event) => {
+        setName(event.target.value)
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const user = {
+            username: name,
+            email: email,
+            password: password
         }
+        onNewUserSubmit(user)
+        setName("")
+        setEmail("")
+        setPassword("")
+    }
 
-        const handleEmailChange = (event) => {
-            setEmail(event.target.value)
-        }
-
-        const handlePasswordChange = (event) => {
-            setPassword(event.target.value)
-        }
-
-        const handleSubmit = (event) => {
-            event.preventDefault()
-            const user = {
-                name: name,
-                email: email,
-                password: password
-            }
-            onNewUserSubmit(user)
-            setName("")
-            setEmail("")
-            setPassword("")
-        }
-
-        return(
-            <>
+    return (
+        <>
             <div className="new-user-form">
                 <h4>CREATE NEW USER</h4>
                 <form className="form-container" onSubmit={handleSubmit}>
-                    <input className="form-inputs" type="text" id="name" placeholder="Enter Name.." value={name} onChange={handleNameChange} required/>
-                    <input className="form-inputs" type="text" id="email" placeholder="Enter Email.." value={email} onChange={handleEmailChange} required/>
-                    <input className="form-inputs" type="text" id="password" placeholder="Password.." value={password} onChange={handlePasswordChange} required/>
+                    <input className="form-inputs" type="text" id="name" placeholder="Enter Name.." value={name} onChange={handleNameChange} required />
+                    <input className="form-inputs" type="text" id="email" placeholder="Enter Email.." value={email} onChange={handleEmailChange} required />
+                    <input className="form-inputs" type="text" id="password" placeholder="Password.." value={password} onChange={handlePasswordChange} required />
                     <input type="submit" value="submit" />
                 </form>
             </div>
-            </>
-        )
-    }
+        </>
+    )
+}
 
 
 export default UserForm;
