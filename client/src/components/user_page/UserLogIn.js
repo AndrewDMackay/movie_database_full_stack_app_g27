d071203b00fbbd02366f133b212473e84eae184e
Kieran Shek <kieranshek@gmail.com>
2021-10-25 11:39:48 +0100

    added progress on user login

--- client/src/components/user_page/UserLogIn.js
+++ client/src/components/user_page/UserLogIn.js
@@ -1,50 +1,43 @@
 
 import React, {useState} from "react";
 
-    const UserLogin = ({onLoginSubmit}) => {
+const UserLogin = ({onLoginSubmit}) => {
 
-        const [username, setUsername] = useState("")
-        const [email, setEmail] = useState("")
-        const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
 
-        const handleNameChange = (event) => {
-            setUsername(event.target.value)
-        }
-
-        // const handleEmailChange = (event) => {
-        //     setEmail(event.target.value)
-        // }
-
-        const handlePasswordChange = (event) => {
-            setPassword(event.target.value)
-        }
-
-        const handleSubmit = (event) => {
-            event.preventDefault()
-            const booking = {
-                username: username,
-                email: email,
-                password: password
-            }
-            onBookingSubmit(booking)
-            setName("")
-            setEmail("")
-            setPassword("")
-        }
-
-        return(
-            <>
-            <div className="user-login">
-                <form onSubmit={handleSubmit}>
-                    <input type="text" id="name" placeholder="Enter Username" value={username} onChange={handleNameChange} required/>
-                    <input type="text" id="password" placeholder="Password" value={password} onChange={handlePasswordChange} required/>
-                    <button className="button-submit"><input type="submit" value="Submit" /></button>
-                </form>
-            </div>
-            </>
-        )
+    const handleNameChange = (event) => {
+        setUsername(event.target.value)
     }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const user = {
+            username: username,
+            password: password
+        }
+        onLoginSubmit(user)
+        setUsername("")
+        setPassword("")
+    }
+
+    return(
+        <>
+        <h4>EXISTING USER</h4>
+        <div className="user-login">
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="name" placeholder="Enter Username" value={username} onChange={handleNameChange} required/>
+                <input type="text" id="password" placeholder="Password" value={password} onChange={handlePasswordChange} required/>
+                <button className="button-submit"><input type="submit" value="Log in" /></button>
+            </form>
+        </div>
+        </>
+    )
+}
 
 
 export default UserLogin;
-
