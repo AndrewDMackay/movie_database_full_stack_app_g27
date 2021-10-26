ceab9c0e963990c297681150ffe9e03604d9fba7
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-26 17:01:47 +0100

    AM, 32st commit develop, all branches merged 26 10 21

--- client/src/components/movie_detail/MovieReviewForm.js
+++ client/src/components/movie_detail/MovieReviewForm.js
@@ -7,16 +7,17 @@ function MovieReviewForm() {
 
     return (
             <>
-                <div className="movie-review-form">
-                    <h4>CREATE NEW REVIEW</h4>
-                    <form className="form-container" onSubmit={handleSubmit}>
-                        <input className="form-inputs" type="text" id="comment" placeholder="Enter Comment.." value={comment} onChange={handleCommentChange} required />
-                        <input className="form-inputs" type="text" id="score" placeholder="Enter Score.." value={score} onChange={handleScoreChange} required />
-                        <input type="submit" value="submit" />
-                    </form>
-                </div>
+            <div className="movie-review-form">
+                <h4>CREATE NEW REVIEW</h4>
+                <form className="form-container" onSubmit={handleSubmit}>
+                    <input className="form-inputs" type="text" id="comment" placeholder="Enter Comment.." value={comment} onChange={handleCommentChange} required />
+                    <input className="form-inputs" type="text" id="score" placeholder="Enter Score.." value={score} onChange={handleScoreChange} required />
+                    <input type="submit" value="submit" />
+                </form>
+            </div>
             </>
             )
 };
 
-            export default MovieReviewForm
+export default MovieReviewForm
+
