2f24487fbb990b22794b09a26344f3ed4977ce86
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-26 15:09:58 +0100

    AM, 7th commit feature/nesting_user_in_movie_container_26_10_21

--- client/src/components/movie_detail/MovieReviewList.js
+++ client/src/components/movie_detail/MovieReviewList.js
@@ -8,20 +8,17 @@ import MovieReviewItem from './MovieReviewItem';
 
 const MovieReviewList = ({ selectedMovie, loggedIn, allUsers }) => {
 
-
+            
     const reviewNodes = allUsers.map((user, index) => {
-
-
-        const filteredReviewItems = reviewNodes.filter((review) => review.movie === selectedMovie.Title)
-
-        return <MovieReviewItem user={user} key={index} />
+        return <MovieReviewItem user={user} key={index}/>
     })
+
 
     return (
         <>
             <div className="movie-review-list-container">
                 <h3>THIS IS THE MOVIE REVIEW LIST</h3>
-                {/* {reviewNodes} */}
+                {reviewNodes}
             </div>
         </>
     )
