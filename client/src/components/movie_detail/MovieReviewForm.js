ace072834917ee873afbdb74403e49662f6ffdec
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-27 12:07:52 +0100

    AM, 37th commit develop, post merging all branches, 27 10 21

--- client/src/components/movie_detail/MovieReviewForm.js
+++ client/src/components/movie_detail/MovieReviewForm.js
@@ -25,12 +25,10 @@ const handleSubmit = (event) => {
         score: score,
         movie: selectedMovie.Title
     }
-    // console.log(newReview);
-    // console.log(loggedIn);
+
     const copyUser = {...loggedIn}
     loggedIn.review.push(newReview)
     onNewReviewSubmit(copyUser)
-    // console.log(loggedIn);
     setComment("")
     setScore(0)
 }
@@ -41,7 +39,7 @@ const handleSubmit = (event) => {
                 <h4>CREATE NEW REVIEW</h4>
                 <form className="form-container" onSubmit={handleSubmit}>
                     <input className="form-inputs" type="text" id="comment" placeholder="Enter Comment.." value={comment} onChange={handleCommentChange} required />
-                    <input className="form-inputs" type="number" id="score" placeholder="Enter Score.." value={score} onChange={handleScoreChange} required />
+                    <input className="form-inputs" type="number" min="1" max="5" id="score" placeholder="Enter Score.." value={score} onChange={handleScoreChange} required />
                     <input type="submit" value="submit" />
                 </form>
             </div>
